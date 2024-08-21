c65991697293e500d51909d2a9eb22a9f38d7d20
shahd-moh-abdel <shahd.mohammed.abdel@gmail.com>
2024-08-21 20:48:22 +0300

    Done!

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Dithering
+
+the algorithm reduces the number of colors in the image, but in a way that keeps the overall look and feel intact
+
+# **Resources**
+
+If you want to learn more about how this dithering thing works, there are a few resources that really helped me understand it better:
+[Floyd-Steinberg Dithering](https://youtu.be/0L2n8Tg2FwI?si=ysJwF5qJFf9286ZQ)
+[Floyd–Steinberg dithering on Wikipedia](https://en.wikipedia.org/wiki/Floyd%E2%80%93)
+[Dither on Wikipedia](https://en.wikipedia.org/wiki/Dither)
--- main.js
+++ main.js
@@ -9,36 +9,61 @@ const ctx = canvas.getContext("2d");
 canvas.width = 220;
 canvas.height = 284;
 
-image.addEventListener("load", (e) => {
+image.addEventListener("load", () => {
   ctx.drawImage(image, 0, 0);
   const pixels = ctx.getImageData(0, 0, canvas.width, canvas.height);
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  const mappedImage = [];
+  function index(x, y) {
+    return (y * canvas.width + x) * 4;
+  }
+
+  function applyErrorDiffusion(x, y, errR, errG, errB) {
+    if (x + 1 < canvas.width) {
+      pixels.data[index(x + 1, y)] += (errR * 7) / 16;
+      pixels.data[index(x + 1, y) + 1] += (errG * 7) / 16;
+      pixels.data[index(x + 1, y) + 2] += (errB * 7) / 16;
+    }
+    if (x - 1 >= 0 && y + 1 < canvas.height) {
+      pixels.data[index(x - 1, y + 1)] += (errR * 3) / 16;
+      pixels.data[index(x - 1, y + 1) + 1] += (errG * 3) / 16;
+      pixels.data[index(x - 1, y + 1) + 2] += (errB * 3) / 16;
+    }
+    if (y + 1 < canvas.height) {
+      pixels.data[index(x, y + 1)] += (errR * 5) / 16;
+      pixels.data[index(x, y + 1) + 1] += (errG * 5) / 16;
+      pixels.data[index(x, y + 1) + 2] += (errB * 5) / 16;
+    }
+    if (x + 1 < canvas.width && y + 1 < canvas.height) {
+      pixels.data[index(x + 1, y + 1)] += (errR * 1) / 16;
+      pixels.data[index(x + 1, y + 1) + 1] += (errG * 1) / 16;
+      pixels.data[index(x + 1, y + 1) + 2] += (errB * 1) / 16;
+    }
+  }
+
   for (let y = 0; y < canvas.height; y++) {
-    let row = [];
-
     for (let x = 0; x < canvas.width; x++) {
-      const index = (y * canvas.width + x) * 4;
-      let red = pixels.data[index];
-      let green = pixels.data[index + 1];
-      let blue = pixels.data[index + 2];
+      const oldRed = pixels.data[index(x, y)];
+      const oldGreen = pixels.data[index(x, y) + 1];
+      const oldBlue = pixels.data[index(x, y) + 2];
 
-      red = Math.round(red / 255) * 255;
-      green = Math.round(green / 255) * 255;
-      blue = Math.round(blue / 255) * 255;
+      const factor = 4;
 
-      const cell = { color: `rgb(${red}, ${green}, ${blue})` };
+      const newRed = Math.round((factor * oldRed) / 255) * (255 / factor);
+      const newGreen = Math.round((factor * oldGreen) / 255) * (255 / factor);
+      const newBlue = Math.round((factor * oldBlue) / 255) * (255 / factor);
 
-      row.push(cell);
+      const errR = oldRed - newRed;
+      const errG = oldGreen - newGreen;
+      const errB = oldBlue - newBlue;
+
+      pixels.data[index(x, y)] = newRed;
+      pixels.data[index(x, y) + 1] = newGreen;
+      pixels.data[index(x, y) + 2] = newBlue;
+
+      applyErrorDiffusion(x, y, errR, errG, errB);
     }
-    mappedImage.push(row);
   }
 
-  for (let y = 0; y < mappedImage.length; y++) {
-    for (let x = 0; x < mappedImage[y].length; x++) {
-      ctx.fillStyle = mappedImage[y][x].color;
-      ctx.fillRect(x, y, 1, 1);
-    }
-  }
+  ctx.putImageData(pixels, 0, 0);
 });
